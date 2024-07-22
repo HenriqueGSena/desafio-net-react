fae6345ad7e25ee04f41503b0f129b69e061fa03
henrique.sena <carloshenrique3250@gmail.com>
2024-07-22 17:40:33 -0300

    main
    * Implmentando documentaocao com swagger;
    * Detalhando endpoints para documento;
    
    Signed-off-by: henrique.sena <carloshenrique3250@gmail.com>

--- webApi/Controller/StudentController.cs
+++ webApi/Controller/StudentController.cs
@@ -3,15 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using webApi.Data;
 using webApi.Entities;
 using webApi.viewModels;
-namespace webApi.Controller 
-{
+namespace webApi.Controller {
     [ApiController]
-    public class StudentController : ControllerBase 
-    {
+    public class StudentController : ControllerBase {
+        /// <summary>
+        ///     Obter lista estudantes
+        /// </summary>
+        /// <returns>Lista Estudantes</returns>
+        /// <response code="200">Sucess</response>
         [HttpGet]
         [Route("/api/students")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAllStudentsAsync(
-            [FromServices] ApplicationDbContext context)
+            [FromServices] ApplicationDbContext context
+        )
         {
             var students = await context
                 .Students
@@ -20,11 +25,20 @@ namespace webApi.Controller
             return Ok(students);
         }
 
+        /// <summary>
+        ///     Obter estudante Id
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <response code="200">Sucess</response>
+        /// <response code="204">Not Content</response>
         [HttpGet]
         [Route("/api/students/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> GetByIdAsync(
-            [FromServices] ApplicationDbContext context, 
-            [FromRoute] int id)
+            [FromServices] ApplicationDbContext context,
+            [FromRoute] int id
+        )
         {
             var student = await context
                 .Students
@@ -33,11 +47,19 @@ namespace webApi.Controller
             return student == null ? Ok(null) : Ok(student);
         }
 
+        /// <summary>
+        ///     Cadastrar novo estudante
+        /// </summary>
+        /// <param name="devEvent">Dados do estudante</param>
+        /// <returns>Estudante recem criado</returns>
+        /// <response code="201">Sucess</response>
         [HttpPost]
         [Route("/api/students")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<IActionResult> PostStudentAsync(
-            [FromServices] ApplicationDbContext context, 
-            [FromBody] CreateStudentViewModel model)
+            [FromServices] ApplicationDbContext context,
+            [FromBody] CreateStudentViewModel model
+        )
         {
             if (!ModelState.IsValid)
                 return BadRequest();
@@ -66,12 +88,21 @@ namespace webApi.Controller
             }
         }
 
+        /// <summary>
+        ///     Atualizar dados estudante
+        /// </summary>
+        /// <param name="id">Identificador do estudante</param>
+        /// <response code="204">Sucess</response>
+        /// <response code="404">Not Found</response>
         [HttpPut]
         [Route("/api/students/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> PutStudentAsync(
-            [FromServices] ApplicationDbContext context, 
-            [FromBody] CreateStudentViewModel model, 
-            [FromRoute] int id)
+            [FromServices] ApplicationDbContext context,
+            [FromBody] CreateStudentViewModel model,
+            [FromRoute] int id
+        )
         {
             if (!ModelState.IsValid)
                 return BadRequest();
@@ -104,11 +135,20 @@ namespace webApi.Controller
             }
         }
 
+        /// <summary>
+        ///     Deletar estudante Id
+        /// </summary>
+        /// <param name="id">Identificador do estudante</param>
+        /// <response code="204">Sucess</response>
+        /// <response code="404">Not Found</response>
         [HttpDelete]
         [Route("/api/students/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteStudentAsync(
-            [FromServices] ApplicationDbContext context, 
-            [FromRoute] int id)
+            [FromServices] ApplicationDbContext context,
+            [FromRoute] int id
+        )
         {
             var student = await context
                 .Students
--- webApi/Program.cs
+++ webApi/Program.cs
@@ -1,13 +1,45 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using webApi.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Desafio-net-react",
+        Version = "v1",
+        Description = "Criação de uma WebAPI com .NET 6 e Front-end em React com Autenticação",
+        Contact = new OpenApiContact
+        {
+            Name = "Carlos Henrique Gomes de Sena",
+            Email = "carloshenrique3250@gmail.com",
+            Url = new Uri("https://henriquegsena.github.io/"),
+            Extensions = new Dictionary<string, IOpenApiExtension>
+            {
+                { "Telefone", new OpenApiString("+55 (21) 99283-4373") }
+            }
+        }
+    });
+
+    string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 using (var scope = app.Services.CreateScope())
 {
